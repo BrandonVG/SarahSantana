fa75ee4adff43bdd6b8106088b4a2d920e2fb9b7
BrandonVG <brandon.vadillox3@gmail.com>
2025-03-13 20:14:05 -0600

    Start

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,22 @@
-import { DataType } from 'sequelize';
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database';
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
