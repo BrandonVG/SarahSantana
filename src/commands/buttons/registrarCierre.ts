ba1f8e68808c001545e0eda796b7600a4ebf516e
BrandonVG <brandon.vadillox3@gmail.com>
2025-03-22 12:59:11 -0600

    Automatically close working employees

--- src/commands/buttons/registrarCierre.ts
+++ src/commands/buttons/registrarCierre.ts
@@ -1,8 +1,10 @@
-import { ButtonInteraction, EmbedBuilder, MessageFlags } from 'discord.js';
+import { ButtonInteraction, EmbedBuilder, GuildMemberRoleManager, MessageFlags } from 'discord.js';
 import LocalRegistry from '../../models/LocalRegistry';
 import moment from 'moment-timezone';
 import prettyMilliseconds from '../../utils/prettyMilliseconds';
 import Role from '../../models/Role';
+import Employee from '../../models/Employee';
+import HoursRegistry from '../../models/HoursRegistry';
 
 export default {
   data: {
@@ -12,18 +14,35 @@ export default {
   async execute(interaction: ButtonInteraction){
     try{
       const employeeRole = await Role.findOne({ where: { guildId: interaction.guildId, isEmployee: true } });
-      const lastRegistry = await LocalRegistry.findOne({ where: { guildId: interaction.guildId }, order: [['startTime', 'DESC']] });
+      const lastRegistry = await LocalRegistry.findOne({ where: { guildId: interaction.guildId, endTime: null }, order: [['startTime', 'DESC']] });
       if (!lastRegistry) return await interaction.reply({ content: 'No hay ningun registro de apertura, haz la apertura, si ya se hizo una apertura contacta a directiva para reportar el error', flags: MessageFlags.Ephemeral });
+      const workingEmployees = await Employee.findAll({ where: { guildId: interaction.guildId, isWorking: true } });
+      const workingEmployeesIds = workingEmployees.map(e => e.discordId);
+      const workingEmployeesRegistries = await HoursRegistry.findAll({ where: { guildId: interaction.guildId, endTime: null, employeeId: workingEmployeesIds } });
       const endTime = new Date();
       lastRegistry.endTime = endTime;
       lastRegistry.workedHours = lastRegistry.endTime.getTime() - lastRegistry.startTime.getTime();
       await lastRegistry.save();
+      for (const registry of workingEmployeesRegistries){
+        registry.endTime = endTime;
+        registry.workedHours = registry.endTime.getTime() - registry.startTime.getTime();
+        await registry.save();
+      }
+      const workingRole = await Role.findOne({ attributes: ['roleId'], where: { isWorking: true, guildId: interaction.guildId } });
+      for (const employee of workingEmployees){
+        employee.isWorking = false;
+        await employee.save();
+        const member = await interaction.guild?.members.fetch(employee.discordId);
+        if (workingRole && member && member.roles.cache.has(workingRole?.roleId)){
+          await member.roles.remove(workingRole.roleId);
+        }
+      }
       const spainTime = moment(endTime).tz('Europe/Madrid').format('HH:mm');
       const embed = new EmbedBuilder()
-      .setColor("#FFFFFF")
-      .setTitle('Cierre registrado correctamente')
-      .addFields({ name: 'Mensaje', value: 'Taller cerrado'})
-      .setDescription('```Hora de cierre: '+ spainTime  +'\nTotal de horas: ' + prettyMilliseconds(lastRegistry.workedHours) + '```');
+        .setColor("#FFFFFF")
+        .setTitle('Cierre registrado correctamente')
+        .addFields({ name: 'Mensaje', value: 'Taller cerrado'})
+        .setDescription('```Hora de cierre: '+ spainTime  +'\nTotal de horas: ' + prettyMilliseconds(lastRegistry.workedHours) + '```');
       const closeMessage = await interaction.reply({ content: `<@&${employeeRole?.roleId}>`, embeds: [embed] });
       setTimeout(async () => {
         if (!interaction.channel) return;
--- src/commands/buttons/registrarEntrada.ts
+++ src/commands/buttons/registrarEntrada.ts
@@ -2,6 +2,7 @@ import { ButtonInteraction, GuildMember, GuildMemberRoleManager, MessageFlags } 
 import Employee from '../../models/Employee';
 import HoursRegistry from '../../models/HoursRegistry';
 import Role from '../../models/Role';
+import LocalRegistry from '../../models/LocalRegistry';
 
 export default {
   data: {
@@ -12,9 +13,11 @@ export default {
     try{
       const member = interaction.member as GuildMember;
       const employee = await Employee.findOne({ where: { discordId: member.id, guildId: interaction.guildId } });
-      const workingRole = await Role.findOne({ attributes: ['roleId'], where: { isWorking: true, guildId: interaction.guildId } });
       if (!employee) return await interaction.reply({ content: 'No estas registrado como empleado, por favor contacta a directiva para registrarte.', flags: MessageFlags.Ephemeral });
       if (employee.isWorking) return await interaction.reply({ content: 'Ya estas trabajando, no puedes registrar tu entrada nuevamente.', flags: MessageFlags.Ephemeral });
+      const lastLocalRegistry = await LocalRegistry.findOne({ where: { guildId: interaction.guildId, endTime: null }, order: [['startTime', 'DESC']] });
+      if (!lastLocalRegistry) return await interaction.reply({ content: 'El local no esta abierto, por favor realiza primero la apertura del local para poder registrar tu ingreso.', flags: MessageFlags.Ephemeral });
+      const workingRole = await Role.findOne({ attributes: ['roleId'], where: { isWorking: true, guildId: interaction.guildId } });
       employee.isWorking = true;
       await employee.save();
       await HoursRegistry.create({ employeeId: employee.discordId, startTime: new Date(), guildId: interaction.guildId });
