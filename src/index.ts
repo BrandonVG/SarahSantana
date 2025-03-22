88c8b919945a7f3d7dfe025696cfecdf5db0a442
BrandonVG <brandon.vadillox3@gmail.com>
2025-03-22 13:03:11 -0600

    little fix

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const client = new SaraClient({ intents: [GatewayIntentBits.Guilds, GatewayInten
   await loadEvents(client);
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
+    await sequelize.sync({ alter: true });
     console.log('Connection to the database has been established successfully.');
   }
   catch(error){
